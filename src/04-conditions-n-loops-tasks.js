89d4bcbd30fb8d5203b56ab91e62ccf4a74a435a
Nadezhda Klinskaya <npantelyeva@gmail.com>
2023-10-27 01:25:07 +0300

    feat: add solution for 3 tasks

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -143,8 +143,18 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  let b = true;
+  if (rect1.left > rect2.left + rect2.width || rect2.left > rect1.left + rect1.width) {
+    b = false;
+  }
+  if (rect1.width === 0 || rect1.height === 0 || rect2.width === 0 || rect2.height === 0) {
+    b = false;
+  }
+  if ((rect1.top + rect1.height) < rect2.top || (rect2.top + rect2.height) < rect1.top) {
+    b = false;
+  }
+  return b;
 }
 
 
@@ -174,8 +184,12 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  if (Math.sqrt(((circle.center.x - point.x) ** 2)
+  + ((circle.center.y - point.y) ** 2)) >= circle.radius) {
+    return false;
+  }
+  return true;
 }
 
 
@@ -569,8 +583,80 @@ function getMatrixProduct(m1, m2) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
+function evaluateTicTacToePosition(position) {
+  let k = 0;
+  let n = 0;
+  for (let i = 0; i < position.length; i += 1) {
+    for (let j = 0; j < position[i].length; j += 1) {
+      if (position[i][j] === '0') {
+        n += 1;
+      } else if (position[i][j] === 'X') {
+        k += 1;
+      }
+    }
+    if (k === 3) {
+      return 'X';
+    }
+    if (n === 3) {
+      return '0';
+    }
+    k = 0;
+    n = 0;
+  }
+  for (let j = 0; j < position[0].length; j += 1) {
+    for (let i = 0; i < position.length; i += 1) {
+      if (position[i][j] === '0') {
+        n += 1;
+      } else if (position[i][j] === 'X') {
+        k += 1;
+      }
+    }
+    if (k === 3) {
+      return 'X';
+    }
+    if (n === 3) {
+      return '0';
+    }
+    k = 0;
+    n = 0;
+  }
+  for (let i = 0; i < position.length; i += 1) {
+    for (let j = 0; j < position[i].length; j += 1) {
+      if (position[i][j] === '0' && i === j) {
+        n += 1;
+      } else if (position[i][j] === 'X' && i === j) {
+        k += 1;
+      }
+    }
+    if (k === 3) {
+      k = 0;
+      return 'X';
+    }
+    if (n === 3) {
+      n = 0;
+      return '0';
+    }
+  }
+  k = 0;
+  n = 0;
+  for (let i = 0; i < position.length; i += 1) {
+    for (let j = 0; j < position[i].length; j += 1) {
+      if (position[i][j] === '0' && i + j === position.length - 1) {
+        n += 1;
+      } else if (position[i][j] === 'X' && i + j === position.length - 1) {
+        k += 1;
+      }
+    }
+    if (k === 3) {
+      k = 0;
+      return 'X';
+    }
+    if (n === 3) {
+      n = 0;
+      return '0';
+    }
+  }
+  return undefined;
 }
 
 
